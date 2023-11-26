b6eff780751d426f79c95cde90c3548bfac01603
Rabia Kambur <genisrabia@gmail.com>
2023-11-26 16:14:47 +0300

    Add manuel fragment transaction

--- app/src/main/java/com/rabiakambur/izmirsukesintitakip/MainActivity.kt
+++ app/src/main/java/com/rabiakambur/izmirsukesintitakip/MainActivity.kt
@@ -3,13 +3,21 @@ package com.rabiakambur.izmirsukesintitakip
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rabiakambur.izmirsukesintitakip.databinding.ActivityMainBinding
+import com.rabiakambur.izmirsukesintitakip.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frameLayoutContainer, MainFragment())
+                .commit()
+        }
     }
 }
